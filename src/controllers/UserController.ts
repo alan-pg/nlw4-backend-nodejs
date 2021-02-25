e0ad0cc776f6f678024411606b091eaa419ce236
Allan <alan92goncalves@gmail.com>
2021-02-25 18:37:52 -0300

    fim do dia 3

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UsersRepository";
+
+class UserContoller {
+  async create(req: Request, res: Response) {
+    const { name, email } = req.body;
+
+    const userRepository = getCustomRepository(UserRepository);
+
+    const userAlreadyExists = await userRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({ error: "User already exists!" });
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+    });
+
+    await userRepository.save(user);
+
+    return res.status(201).json(user);
+  }
+}
+
+export { UserContoller };
+
